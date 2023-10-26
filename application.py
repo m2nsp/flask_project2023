cf65b18c5e888ee8a6a0f6185a3f3b61198c7f2a
정유정 <8w8u8@ewhain.net>
2023-10-26 14:20:04 +0000

    일단 교수님 ppt내용

--- application.py
+++ application.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template, request
 
+application = Flask(__name__)
+
+@application.route("/", methods=['GET', 'POST'])
+def hello():
+    return render_template("index.html")
+
+@application.route("/list_post", methods=['GET', 'POST'])
+def view_list():
+    return render_template("list.html")
+
+@application.route("/review_post", methods=['GET', 'POST'])
+def view_review():
+    return render_template("review.html")
+
+@application.route("/reg_items_post", methods=['GET', 'POST'])
+def reg_item():
+    return render_template("reg_items.html")
+
+@application.route("/reg_reviews_post", methods=['GET', 'POST'])
+def reg_review():
+    return render_template("reg_reviews.html")
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
