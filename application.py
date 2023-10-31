770b7d4862ec5df08986a69e89ea326e5b46c57d
정유정 <8w8u8@ewhain.net>
2023-10-31 04:25:59 +0000

    week8 수정중, 터미널에 뜨는것까지 확인

--- application.py
+++ application.py
@@ -2,55 +2,64 @@ from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
-<<<<<<< HEAD
 @application.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@application.route("/list_post", methods=['GET', 'POST'])
-def view_list():
-    return render_template("list.html")
+@application.route("/productList")
+def productList():
+    return render_template("productList.html")
 
-@application.route("/review_post", methods=['GET', 'POST'])
-def view_review():
-    return render_template("review.html")
+@application.route("/productRegister")
+def productRegister():
+    return render_template("productRegister.html")
 
-@application.route("/reg_items_post", methods=['GET', 'POST'])
-def reg_item():
-    return render_template("reg_items.html")
+@application.route("/submit", methods=['POST'])
+def submitProduct():
+    if request.method == "POST":
+        product_title = request.form.get("product-title")
+        price_method = request.form.get("price-method")
+        product_description = request.form.get("product-description")
+        user_id = request.form.get("user-id")
+        post_date = request.form.get("post-date")
 
-@application.route("/reg_reviews_post", methods=['GET', 'POST'])
-def reg_review():
-    return render_template("reg_reviews.html")
+        if price_method == "일반거래":
+            normal_price = request.form.get("normal-price")
+        elif price_method == "경매":
+            auction_end_time = request.form.get("auction-end-time")
+            auction_min_bid = request.form.get("auction-min-bid")
+            auction_max_bid = request.form.get("auction-max-bid")
+
+        #터미널에 데이터값 출력
+        print("상품명(글제목):", product_title)
+        print("가격방식:", price_method)
+
+        if price_method == "일반거래":
+            print("판매가:", normal_price)
+        elif price_method == "경매":
+            print("경매마김일:", auction_end_time)
+            print("최저낙찰가:", auction_min_bid)
+            print("최고낙찰가:", auction_max_bid)
+        
+        print("상세설명:", product_description)
+        print("글작성날짜:", post_date)
+
+    return "상품이 성공적으로 등록되었습니다."
+
+
+
+@application.route("/reviewRegister")
+def reviewRegister():
+    return render_template("reviewRegister.html")
+
+@application.route("/myReview")
+def myReview():
+    return render_template("myReview.html")
+
+@application.route("/productSubmitResult")
+def productSubmitResult():
+    return render_template("productSubmitResult.html")
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
-=======
-@application.route("/", methods=['POST'])
-def hello():
-    return render_template("index.html")
-
-@application.route("/list_post", methods=['POST'])
-def view_list():
-    return render_template("list.html")
-
-@application.route("/review_post", methods=['POST'])
-def view_review():
-    return render_template("review.html")
-
-@application.route("/reg_items_post", methods=['POST'])
-def reg_item():
-    return render_template("reg_items.html")
-
-@application.route("/reg_reviews_post", methods=['POST'])
-def reg_review():
-    return render_template("reg_reviews.html")
-
-@application.route("/submit_item_post", methods=['POST'])
-def reg_item_submit_post():
-    data=request.form
-    return render_template("submit_item_result.html", data=data)
-
-if __name__ == "__main__":
-    application.run(host='0.0.0.0')
->>>>>>> 688ddb7cfa548f38c55376f30066cc8aedb034f0
